691ec642fc756efc5f784b4c2037190c7f4a7b96
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-07-05 23:53:20 +0530

    enabled routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import { AllRoutes } from "./Components/AllRoutes";
+import { Navbar } from "./Components/Navbar";
 
 function App() {
-  return <></>;
+  return
+  <>
+  <Navbar/>
+  <AllRoutes/>
+  </>;
 }
 
 export default App;
