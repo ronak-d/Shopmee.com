3bf60befa48536dfcb0bfd3ddc0523b57529cec7
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-07-06 01:45:51 +0530

    pages done

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom';
+import { Cartpage } from '../Pages/Cartpage';
+import { Homepage } from '../Pages/Homepage';
+import { Kidspage } from '../Pages/Kidspage';
+import { Menpage } from '../Pages/Menpage';
+import { Shoespage } from '../Pages/Shoespage';
+import { Womenpage } from '../Pages/Womenpage';
 
 export const AllRoutes = () => {
   return (
@@ -7,7 +13,7 @@ export const AllRoutes = () => {
       <Route path='/' element={<Homepage/>}/>
       <Route path='/women' element={<Womenpage/>}/>
       <Route path='/kids' element={<Kidspage/>}/>
-      <Route path='/mens' element={<Menspage/>}/>
+      <Route path='/mens' element={<Menpage/>}/>
       <Route path='/shoes' element={<Shoespage/>}/>
       <Route path='/cart' element={<Cartpage/>}/>
     </Routes>
