4c6c786e607ec43af7d17a8f24d7ef690c91ff74
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-07-08 03:43:10 +0530

    redux

---
+++ src/Pages/ProductPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Box} from "@chakra-ui/react";
+import { FilterComponent } from '../Components/FilterComponent';
+
+
+export const ProductPage = () => {
+  return (
+    <Box>
+        <Box>
+            <FilterComponent/>
+        </Box>
+        <Box>
+
+        </Box>
+    </Box>
+  )
+}
---
+++ src/Redux/products/action.js
@@ -0,0 +1,7 @@
+
+
+    const fecthDataRequest = (payload) =>{
+        return {
+            
+        }
+    }
