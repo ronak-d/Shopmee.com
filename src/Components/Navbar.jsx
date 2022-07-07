402c2a71e39e0b3c35d6cb8a3888ac12f5a269e8
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-07-07 22:02:20 +0530

    chakra

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from '@chakra-ui/icons';
+
 export const Navbar = () => {
-  // return (
-    import {
-      Box,
-      Flex,
-      Text,
-      IconButton,
-      Button,
-      Stack,
-      Collapse,
-      Icon,
-      Link,
-      Popover,
-      PopoverTrigger,
-      PopoverContent,
-      useColorModeValue,
-      useBreakpointValue,
-      useDisclosure,
-    } from '@chakra-ui/react';
-    import {
-      HamburgerIcon,
-      CloseIcon,
-      ChevronDownIcon,
-      ChevronRightIcon,
-    } from '@chakra-ui/icons';
-    
+  return (
     export default function WithSubnavigation() {
       const { isOpen, onToggle } = useDisclosure();
     
