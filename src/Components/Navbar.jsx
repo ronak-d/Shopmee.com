306b088bf37967b415c9153f5f3aee5678659eac
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-07-07 22:48:46 +0530

    done with navigation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -147,99 +147,99 @@ export const Navbar = () => {
     );
   };
   
-  const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
-    return (
-      <Link
-        href={href}
-        role={'group'}
-        display={'block'}
-        p={2}
-        rounded={'md'}
-        _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
-        <Stack direction={'row'} align={'center'}>
-          <Box>
-            <Text
-              transition={'all .3s ease'}
-              _groupHover={{ color: 'pink.400' }}
-              fontWeight={500}>
-              {label}
-            </Text>
-            <Text fontSize={'sm'}>{subLabel}</Text>
-          </Box>
-          <Flex
-            transition={'all .3s ease'}
-            transform={'translateX(-10px)'}
-            opacity={0}
-            _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-            justify={'flex-end'}
-            align={'center'}
-            flex={1}>
-            <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
-          </Flex>
-        </Stack>
-      </Link>
-    );
-  };
+  // const DesktopSubNav = ({ label, href, subLabel }) => {
+  //   return (
+  //     <Link
+  //       href={href}
+  //       role={'group'}
+  //       display={'block'}
+  //       p={2}
+  //       rounded={'md'}
+  //       _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+  //       <Stack direction={'row'} align={'center'}>
+  //         <Box>
+  //           <Text
+  //             transition={'all .3s ease'}
+  //             _groupHover={{ color: 'pink.400' }}
+  //             fontWeight={500}>
+  //             {label}
+  //           </Text>
+  //           <Text fontSize={'sm'}>{subLabel}</Text>
+  //         </Box>
+  //         <Flex
+  //           transition={'all .3s ease'}
+  //           transform={'translateX(-10px)'}
+  //           opacity={0}
+  //           _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+  //           justify={'flex-end'}
+  //           align={'center'}
+  //           flex={1}>
+  //           <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+  //         </Flex>
+  //       </Stack>
+  //     </Link>
+  //   );
+  // };
   
-  const MobileNav = () => {
-    return (
-      <Stack
-        bg={useColorModeValue('white', 'gray.800')}
-        p={4}
-        display={{ md: 'none' }}>
-        {NAV_ITEMS.map((navItem) => (
-          <MobileNavItem key={navItem.label} {...navItem} />
-        ))}
-      </Stack>
-    );
-  };
+  // const MobileNav = () => {
+  //   return (
+  //     <Stack
+  //       bg={useColorModeValue('white', 'gray.800')}
+  //       p={4}
+  //       display={{ md: 'none' }}>
+  //       {NAV_ITEMS.map((navItem) => (
+  //         <MobileNavItem key={navItem.label} {...navItem} />
+  //       ))}
+  //     </Stack>
+  //   );
+  // };
   
-  const MobileNavItem = ({ label, children, href }: NavItem) => {
-    const { isOpen, onToggle } = useDisclosure();
+  // const MobileNavItem = ({ label, children, href }: NavItem) => {
+  //   const { isOpen, onToggle } = useDisclosure();
   
-    return (
-      <Stack spacing={4} onClick={children && onToggle}>
-        <Flex
-          py={2}
-          as={Link}
-          href={href ?? '#'}
-          justify={'space-between'}
-          align={'center'}
-          _hover={{
-            textDecoration: 'none',
-          }}>
-          <Text
-            fontWeight={600}
-            color={useColorModeValue('gray.600', 'gray.200')}>
-            {label}
-          </Text>
-          {children && (
-            <Icon
-              as={ChevronDownIcon}
-              transition={'all .25s ease-in-out'}
-              transform={isOpen ? 'rotate(180deg)' : ''}
-              w={6}
-              h={6}
-            />
-          )}
-        </Flex>
+  //   return (
+  //     <Stack spacing={4} onClick={children && onToggle}>
+  //       <Flex
+  //         py={2}
+  //         as={Link}
+  //         href={href ?? '#'}
+  //         justify={'space-between'}
+  //         align={'center'}
+  //         _hover={{
+  //           textDecoration: 'none',
+  //         }}>
+  //         <Text
+  //           fontWeight={600}
+  //           color={useColorModeValue('gray.600', 'gray.200')}>
+  //           {label}
+  //         </Text>
+  //         {children && (
+  //           <Icon
+  //             as={ChevronDownIcon}
+  //             transition={'all .25s ease-in-out'}
+  //             transform={isOpen ? 'rotate(180deg)' : ''}
+  //             w={6}
+  //             h={6}
+  //           />
+  //         )}
+  //       </Flex>
   
-        <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-          <Stack
-            mt={2}
-            pl={4}
-            borderLeft={1}
-            borderStyle={'solid'}
-            borderColor={useColorModeValue('gray.200', 'gray.700')}
-            align={'start'}>
-            {children &&
-              children.map((child) => (
-                <Link key={child.label} py={2} href={child.href}>
-                  {child.label}
-                </Link>
-              ))}
-          </Stack>
-        </Collapse>
-      </Stack>
-    );
-  };
+  //       <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+  //         <Stack
+  //           mt={2}
+  //           pl={4}
+  //           borderLeft={1}
+  //           borderStyle={'solid'}
+  //           borderColor={useColorModeValue('gray.200', 'gray.700')}
+  //           align={'start'}>
+  //           {children &&
+  //             children.map((child) => (
+  //               <Link key={child.label} py={2} href={child.href}>
+  //                 {child.label}
+  //               </Link>
+  //             ))}
+  //         </Stack>
+  //       </Collapse>
+  //     </Stack>
+  //   );
+  // };
