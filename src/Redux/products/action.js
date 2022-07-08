96a52b63ce0d2f4732a42afdc2f5ea49cdc63781
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-07-09 02:13:09 +0530

    action formed

--- src/Redux/products/action.js
+++ src/Redux/products/action.js
@@ -1,7 +1,33 @@
+import axios, { Axios } from 'axios';
+import * as types from './actionTypes';
 
-
-    const fecthDataRequest = (payload) =>{
+    const fetchDataRequest = (payload) =>{
         return {
-            
+            type:types.FETCH_DATA_REQUEST,
+            payload
         }
+    }
+    
+    const fetchDataSuccess = (payload) =>{
+        return {
+            type:types.FETCH_DATA_SUCCESS,
+            payload
+        }
+    }
+    const fetchDatafailure = (payload) =>{
+        return {
+            type:types.FETCH_DATA_FAILURE,
+            payload
+        }
+    }
+
+    const fetchdata = (payload) =>{
+        return (dispatch) =>{
+
+            dispatch(fetchDataRequest());
+            
+            axios.get("./products")
+            .then((response) => dispatch(fetchDataSuccess(response.data)))
+            .catch((error) => dispatch(fetchDatafailure(err.data)))
+        };
     }
