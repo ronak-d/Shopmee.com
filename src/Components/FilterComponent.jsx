8b6f7daceef6604172f6fbe143afff68746494f0
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-07-10 23:29:19 +0530

    categoryHandler

--- src/Components/FilterComponent.jsx
+++ src/Components/FilterComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box} from '@chakra-ui/react';
+import {Box, VStack} from '@chakra-ui/react';
 
 export const FilterComponent = () => {
   return (
@@ -7,6 +7,15 @@ export const FilterComponent = () => {
       <Box>
         <Text>Filters</Text>
         <Text>Cateogary</Text>
+
+        <CheckboxGroup colorScheme='green' defaultValue={['naruto', 'kakashi']} onChange={categoryHandler}>
+          <VStack>
+            <Checkbox value='naruto'>Naruto</Checkbox>
+            <Checkbox value='sasuke'>Sasuke</Checkbox>
+            <Checkbox value='kakashi'>Kakashi</Checkbox>
+          </VStack>
+        </CheckboxGroup>
+
         <Text></Text>
 
       </Box>
